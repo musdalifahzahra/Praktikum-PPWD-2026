0b446ce0ef9e928394e704e4f91c5d7dfecc6bba
musdalifahzahra <124250135@student.upnyk.ac.id>
2026-05-13 14:52:54 +0700

    Up File

---
+++ 124250135_latihan_responsi/peminjaman_laboratorium.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Bulan Mei 2026 pada 09.51
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `peminjaman_laboratorium`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jam`
+--
+
+CREATE TABLE `jam` (
+  `id_jam` int(5) NOT NULL,
+  `jam` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `jam`
+--
+
+INSERT INTO `jam` (`id_jam`, `jam`) VALUES
+(1, '08:00'),
+(2, '10:30'),
+(3, '13:00'),
+(4, '15:30');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `laboratorium`
+--
+
+CREATE TABLE `laboratorium` (
+  `id_laboratorium` int(3) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `laboratorium`
+--
+
+INSERT INTO `laboratorium` (`id_laboratorium`, `nama`) VALUES
+(1, 'Basis Data'),
+(2, 'PPSI'),
+(3, 'Teknologi Mobile'),
+(4, 'Komputasi'),
+(5, 'Jaringan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(3) NOT NULL,
+  `id_laboratorium` int(3) DEFAULT NULL,
+  `tanggal` date NOT NULL,
+  `id_jam` int(3) DEFAULT NULL,
+  `id_user` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `riwayat`
+--
+
+CREATE TABLE `riwayat` (
+  `id_riwayat` int(3) NOT NULL,
+  `id_laboratorium` int(3) DEFAULT NULL,
+  `tanggal` date NOT NULL,
+  `id_jam` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `riwayat`
+--
+
+INSERT INTO `riwayat` (`id_riwayat`, `id_laboratorium`, `tanggal`, `id_jam`) VALUES
+(0, 2, '2026-05-13', 4),
+(6, 1, '2026-05-12', 1),
+(7, 1, '2026-05-12', 2),
+(8, 1, '2026-05-12', 3),
+(9, 1, '2026-05-12', 1),
+(10, 2, '2026-05-13', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tersedia`
+--
+
+CREATE TABLE `tersedia` (
+  `id_tersedia` int(3) NOT NULL,
+  `id_lab` int(3) DEFAULT NULL,
+  `id_jam` int(3) DEFAULT NULL,
+  `status` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tersedia`
+--
+
+INSERT INTO `tersedia` (`id_tersedia`, `id_lab`, `id_jam`, `status`) VALUES
+(1, 1, 1, '1'),
+(2, 1, 2, '1'),
+(3, 1, 3, '1'),
+(4, 1, 4, '1'),
+(5, 2, 1, '1'),
+(6, 2, 2, '1'),
+(7, 2, 3, '1'),
+(8, 2, 4, '1'),
+(9, 3, 1, '1'),
+(10, 3, 2, '1'),
+(11, 3, 3, '1'),
+(12, 3, 4, '1'),
+(13, 4, 1, '1'),
+(14, 4, 2, '1'),
+(15, 4, 3, '1'),
+(16, 4, 4, '1'),
+(17, 5, 1, '1'),
+(18, 5, 2, '1'),
+(19, 5, 3, '1'),
+(20, 5, 4, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(3) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `email`, `username`, `password`) VALUES
+(2, 'musdalifah@gmail.com', 'musdalifah', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `jam`
+--
+ALTER TABLE `jam`
+  ADD PRIMARY KEY (`id_jam`);
+
+--
+-- Indeks untuk tabel `laboratorium`
+--
+ALTER TABLE `laboratorium`
+  ADD PRIMARY KEY (`id_laboratorium`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD KEY `fk_peminjaman_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `riwayat`
+--
+ALTER TABLE `riwayat`
+  ADD PRIMARY KEY (`id_riwayat`);
+
+--
+-- Indeks untuk tabel `tersedia`
+--
+ALTER TABLE `tersedia`
+  ADD PRIMARY KEY (`id_tersedia`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `fk_peminjaman_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
